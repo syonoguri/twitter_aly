b6ac090ffdab3c1ebe96e9bfa894e8d7e503da4d
interngc <41315313+interngc@users.noreply.github.com>
2018-09-19 14:28:03 +0900

    commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var path = require("path");
 var morgan = require("morgan");
 var express = require("express");
 
@@ -6,8 +7,11 @@ var app = express();
 
 app.use(morgan("combined"));
 
+app.set("views", path.join(_dirname, "templates" ));
+app.set("view engine", "pug");
+
 app.get("/", function(req, res) {
-    return res.send("Hello, World");
+    return res.render("index", {title: "Hello, World"});
 });
 
 var server = http.createServer(app);
