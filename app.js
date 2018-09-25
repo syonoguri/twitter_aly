a23d815c2d353a3b8ef0b154cd69213ea680d5e6
interngc <41315313+interngc@users.noreply.github.com>
2018-09-25 14:12:37 +0900

    commit

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post("/form", function(req, res) {
     }, function(err, res, body){
         return body;
     });
-    return res.render("result", {result: analyze[1]});
+    return res.render("result", {result: analyze[0]});
 });
 
 var server = http.createServer(app);
