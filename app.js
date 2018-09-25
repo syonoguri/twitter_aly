6d1d87d60675005ef425c7a6b68028cfd6f814f5
interngc <41315313+interngc@users.noreply.github.com>
2018-09-25 16:30:16 +0900

    commit

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/form", function(req, res) {
         console.log(analysisResult);
         var resultArray = [];
         for(var i in analysisResult){
-            resultArray.push(i+":"+analysisResult.i);
+            resultArray.push(i+":"+analysisResult.i.toString());
         }
         console.log(resultArray);
         var resultOutput = resultArray.join(" , ")
