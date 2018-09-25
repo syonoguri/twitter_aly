9b3edbe6f2814a22610b12418d2af800d5d7afae
interngc <41315313+interngc@users.noreply.github.com>
2018-09-25 12:02:41 +0900

    commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express = require("express");
 var app = express();
 var request = require("request")
 
+require("dotenv").config();
 var key = process.env.NODE_KEY;
 
 app.use("/static", express.static(path.join(__dirname,"static")));
