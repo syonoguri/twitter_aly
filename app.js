b9fdd91abd8543952bf50447108fdcad5be0dc25
interngc <41315313+interngc@users.noreply.github.com>
2018-10-03 11:12:12 +0900

    commit

--- app.js
+++ app.js
@@ -44,7 +44,10 @@ app.post("/form", function(req, res) {
         var analysisResultR = JSON.parse(body);
         var resultArrayR = [];
         for(var i=0; i<30; i++){
-            if(analysisResultR["Items"][i] == undefined) break;
+            if(analysisResultR["Items"][i] == undefined) {
+                resultArrayR[0] = "Error:このキーワードでヒットする商品がありません。";
+                break;
+            }    
             resultArrayR.push(analysisResultR["Items"][i]["Item"]["itemName"]);
         }
         var resultOutputR = resultArrayR.join("");
