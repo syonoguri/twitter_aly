4a4ace87db851870e80374bbe4d2162208dbe402
interngc <41315313+interngc@users.noreply.github.com>
2018-10-03 15:11:02 +0900

    commit

--- apptest.js
+++ apptest.js
@@ -5,7 +5,7 @@ var myFunc = function(a,b){
     return a + b;
 };
 
-describe("仮テスト" function(){
+describe("仮テスト", function(){
     it("myFuncのテスト",function(){
         assert.strictEqual(myFunc(1,2),3)
     });
