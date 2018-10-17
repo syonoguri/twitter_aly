5fb0f258a350ff7f19ff0b1bac02df766c397cbe
interngc <41315313+interngc@users.noreply.github.com>
2018-10-17 14:09:00 +0900

    commit

--- appclient.js
+++ appclient.js
@@ -17,7 +17,8 @@ app.use(function(req, res, next) {
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("http://localhost:3000/form", function(req, res) {
+app.post("/form", function(req, res) {
+    res.send("nice");
     if(req.body.sentence=="") {
         res.send("Error: 入力がありません")
     } else if(/^\s+$/.test(req.body.sentence)){
