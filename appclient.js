8f2c294ceec1b42434be863003b1189894bc70fd
interngc <41315313+interngc@users.noreply.github.com>
2018-10-17 11:41:23 +0900

    commit

--- appclient.js
+++ appclient.js
@@ -13,7 +13,6 @@ var rKey = process.env.NODE_RKEY;
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000/form");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
     next();
 });
 app.use(morgan("combined"));
