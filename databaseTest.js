630ed930334781733b8ab5990be78d401de4d119
interngc <41315313+interngc@users.noreply.github.com>
2018-12-26 15:59:56 +0900

    commit

---
+++ databaseTest.js
@@ -0,0 +1,27 @@
+var mysql = require("/usr/local/lib/node_modules/mysql");
+var connection = mysql.createConnection({
+    host:"localhost",
+    database:"rakutenapplication",
+    user:"dbuser",
+    password:"gladcubeogr"
+});
+
+//select
+var query = connection.query("select * from login",function(err,results){
+
+    //display error
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    //get rows
+    for(var i in results){
+        console.log(results[i].name + " " + results[i].password);
+    }
+});
+
+//insert
+var query2 = connection.query("insert into members(name,email) values('test','test@test.com')");
+
+connection.end();
