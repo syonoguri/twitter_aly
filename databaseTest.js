69a71bc03a7840cac4dd25af00988e6f7f87e0d0
interngc <41315313+interngc@users.noreply.github.com>
2018-12-26 16:47:29 +0900

    commit

--- databaseTest.js
+++ databaseTest.js
@@ -1,27 +1,50 @@
-var mysql = require("/usr/local/lib/node_modules/mysql");
-var connection = mysql.createConnection({
-    host:"localhost",
-    database:"rakutenapplication",
-    user:"dbuser",
-    password:"gladcubeogr"
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var app = express();
+var request = require("request")
+var bodyParser = require("body-parser");
+var morgan = require("morgan");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-//select
-var query = connection.query("select * from login",function(err,results){
+app.use(morgan("combined"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-    //display error
-    if(err){
-        console.log(err);
-        return;
-    }
+app.post("/form", function(req, res) {
+    console.log(req);
+    var server = http.createServer(app);
+    server.listen(3000);
 
-    //get rows
-    for(var i in results){
-        console.log(results[i].name + " " + results[i].password);
-    }
-});
+    var mysql = require("/usr/local/lib/node_modules/mysql");
+    var connection = mysql.createConnection({
+        host:"localhost",
+        database:"rakutenapplication",
+        user:"dbuser",
+        password:"gladcubeogr"
+    });
+
+    //select
+    var query = connection.query("select * from login",function(err,results){
+
+        //display error
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        //get rows
+        for(var i in results){
+            console.log(results[i].name + " " + results[i].password);
+        }
+    });
 
 //insert
+    connection.end();
+    res.send("");   
+});
 
-
-connection.end();
