5fc8e15f444013acdc4c6cbd3525f2604f7ca0df
interngc <41315313+interngc@users.noreply.github.com>
2019-01-09 18:44:09 +0900

    commit

--- passportTest.js
+++ passportTest.js
@@ -16,13 +16,14 @@ app.set("views", path.join(__dirname, "templates" ));
 app.set("view engine", "pug");
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: false }));
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
-var session = require("express-session");
+// 既に色々入っているので、そこへ追加
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
 
-// signin ページの追加
-var signinRouter = require(".routes/signin");
-app.use("/signin");
+// signinページの追加
+var signinRouter = require('.routes/signin');
+app.use('/signin');
 
 // session, passport.initialize, passport.sessionは以下の順番で追加
 app.use(session({
@@ -30,46 +31,51 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
-app.use(passport.itinialize());
+app.use(passport.initialize());
 app.use(passport.session());
 
-//authentication
-passport.serializeUser(function(username,done){
-  console.log("serializeUser");
-  done(null,username);
+// authentication
+passport.serializeUser(function(username, done) {
+  console.log('serializeUser');
+  done(null, username);
 });
 
-passport.deserializeUser(function(username,done){
-  console.log("deserializeUser");
-  done(null,{name:username});
+passport.deserializeUser(function(username, done) {
+  console.log('deserializeUser');
+  done(null, {name:username});
 });
 
 passport.use(new LocalStrategy(
   {
-    // signinのformで提議したnameの要素をセット
+    // signinのformで定義したnameの要素をセット
     usernameField: "username",
     passwordField: "password"
   },
-  function(username,password,done){
-    //仮にusernameとpasswordを設置
+  function(username, password, done){
+    // ここでは、データベースを使わずに、仮にusernameとpasswordを固定で入れています。
     if(username == "test" && password == 123456789){
       return done(null, username);
     }
-    return done(null,false, {message: "invalid"});
+    return done(null, false, {message: "invalid"});
   }
 ));
 
-app.post("/signin",
-  passport.authenticate("local",
-  {
-    failureRedirect: "/signin"
-  }
-  ),
-  function(req,res,next){
-    fetch("http://localhost:3000/signin",
+app.post('/signin',
+  passport.authenticate('local',
     {
-      credentials:"include"
+      failureRedirect: "/signin"
     }
+  ),
+  function(req, res, next){
+    // res.redirect("/")でreq.userが渡せなかったので、ここでfetchを使っています。
+    // https://github.com/jaredhanson/passport/issues/244
+    // fetchは以下のようにインストール
+    // npm install --save isomorphic-fetch
+    // var fetch = require('isomorphic-fetch');
+    fetch("http://localhost:3000/signin",
+      {
+        credentials: "include"
+      }
     ).then(function(){
       res.redirect("/");
     }).catch(function(e){
@@ -77,6 +83,5 @@ app.post("/signin",
     });
   }
 );
-
 var server = http.createServer(app);
 server.listen(3000);
