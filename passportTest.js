39553324ee8adffc4f50f7a9a9a63824815a85e3
interngc <41315313+interngc@users.noreply.github.com>
2019-01-09 18:41:08 +0900

    commit

--- passportTest.js
+++ passportTest.js
@@ -2,6 +2,7 @@ var express = require("express");
 var http = require("http");
 var app = express();
 var bodyParser = require("body-parser");
+var path = require("path");
 var morgan = require("morgan");
 var fetch = require("isomorphic-fetch");
 
@@ -10,7 +11,9 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+app.use("/static", express.static(path.join(__dirname,"static")));
+app.set("views", path.join(__dirname, "templates" ));
+app.set("view engine", "pug");
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: false }));
 var passport = require("passport");
