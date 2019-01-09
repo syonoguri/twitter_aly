c579f05a354a30f5169bfc656b992b2177cfe1cd
interngc <41315313+interngc@users.noreply.github.com>
2019-01-09 18:36:20 +0900

    commit

--- passportTest.js
+++ passportTest.js
@@ -18,7 +18,7 @@ var LocalStrategy = require("passport-local").Strategy;
 var session = require("express-session");
 
 // signin ページの追加
-var signinRouter = require(".templates/signin");
+var signinRouter = require("/templates/signin.pug");
 app.use("/signin");
 
 // session, passport.initialize, passport.sessionは以下の順番で追加
