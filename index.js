71a613bd50c3b20a1dbfc325eeb576fb00fc28cc
interngc <41315313+interngc@users.noreply.github.com>
2019-01-10 17:56:02 +0900

    commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var router_index = express.Router();
 
 // index.pugをgetした時にコールバックを実行
 router_index.get('/', function(req, res, next) {
-  console.log(req.user); // {name:'test'}
-  if (req.user) { // ログインしているユーザーが存在する場合のみ有効
+  console.log(req.username); // {name:'test'}
+  if (req.username) { // ログインしているユーザーが存在する場合のみ有効
     console.log("logined")
-    res.render("index", { username: req.user.name});
+    res.render("index", { username: req.username});
   } else { // ユーザーが存在しなければ、サインインページへ飛ばされる
     console.log("notlogined")
     res.redirect("/signin");
