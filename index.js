51b3f4765c6f6676d8882e1ae30ac637366f84a6
interngc <41315313+interngc@users.noreply.github.com>
2019-01-10 12:49:37 +0900

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var router_index = express.Router();
 router_index.get('/', function(req, res, next) {
   console.log(req.user); // {name:'test'}
   if (req.user) { // ログインしているユーザーが存在する場合のみ有効
-    res.render("/index", { username: req.user.name});
+    res.render("index", { username: req.user.name});
   } else { // ユーザーが存在しなければ、サインインページへ飛ばされる
     res.redirect("/signin");
   }
--- signin.js
+++ signin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router_signin = express.Router();
 
 router_signin.get('/signin', function(req, res, next) {
-  res.render("/signin", {});
+  res.render("signin", {});
 });
 
 module.exports = router_signin;
