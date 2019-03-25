af3b2445d2116f1ed217393b5e5b911a7ff90839
interngc <41315313+interngc@users.noreply.github.com>
2019-03-25 10:41:36 +0900

    docker

--- app.js
+++ app.js
@@ -7,34 +7,26 @@ var request = require("request")
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
 
-
 var yKey = process.env.NODE_YKEY;
 var rKey = process.env.NODE_RKEY;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use("/static", express.static(path.join(__dirname,"static")));
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/static", express.static(path.join(__dirname,"static")));
-
-app.set("views", path.join(__dirname, "templates" ));
-app.set("view engine", "pug");
-
-app.get("/", function(req, res) {
-    return res.render("index", {title: "Hello World" });
-});
-app.get("/form", function(req, res) {
-    return res.render("form");
-});
-
-
 app.post("/form", function(req, res) {
-    
+    console.log(req);
     if(req.body.sentence=="") {
-        res.render("form",{result: "Error: 入力がありません"})
-    } else if(req.body.sentence.length>128){
-        res.render("form",{result: "Error: 128文字以内で入力してください"})
+        res.send(JSON.stringify("Error: 入力がありません"));
     } else if(/^\s+$/.test(req.body.sentence)){
-        res.render("form",{result: "Error: スペースのみでの検索はできません"})
+        res.send(JSON.stringify("Error: スペースのみでの検索はできません"));
+    } else if(req.body.sentence.length>128){
+        res.send(JSON.stringify("Error: 128文字以内で入力してください"));
     } else {
         request.get({
             url: "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706",
@@ -44,12 +36,13 @@ app.post("/form", function(req, res) {
             }
         }, function(error,response,body){
         var analysisResultR = JSON.parse(body);
+        console.log(analysisResultR);
         var resultArrayR = [];
         
         // 該当商品が無かった場合の処理
         if(analysisResultR["Items"][0]==undefined) {
             resultArrayR[0] = "Error:このキーワードでヒットする商品がありません。";
-            res.render("form",{result: resultArrayR[0]});
+            res.send(JSON.stringify(resultArrayR[0]));
             return;
         }
 
@@ -61,7 +54,6 @@ app.post("/form", function(req, res) {
         }
         // 配列に格納した商品タイトルを文字列に変換
         var resultOutputR = resultArrayR.join("");
-
         var analysis = request.post({
             url: "https://jlp.yahooapis.jp/KeyphraseService/V1/extract",
             headers: {
@@ -73,14 +65,7 @@ app.post("/form", function(req, res) {
                 output: "json"
             }
         }, function(error, response, body){
-            var analysisResult = JSON.parse(body);
-            var resultArray = [];
-            for(var i in analysisResult){
-                resultArray.push(i+":"+analysisResult[i]);
-            }
-            var resultOutput = resultArray.join(" , ");
-            console.log(analysisResult);
-            res.render("form",{result: resultOutput});
+            res.send(JSON.parse(body));
         }); 
     });
     }
